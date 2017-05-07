184d7030bf1a274ad73654af5f158491c530899d
Matt Harris <mattharris89@gmail.com>
2017-05-07 18:05:36 -0400

    getting XML parsed to json adding package.json and yarn lock file w/ .gitignore

--- src/get-icon.js
+++ src/get-icon.js
@@ -1,23 +1,30 @@
 const https = require('https');
+const xml2js = require('xml2js');
+const StringDecoder = require('string_decoder').StringDecoder;
 
 class GetIcon {
-	constructor() {
+	constructor(iconName) {
 		this.options = {
 			hostname: 'storage.googleapis.com',
 			port: 443,
-			path: '/material-icons/external-assets/v4/icons/svg/ic_accessibility_black_24px.svg',
+			path: `/material-icons/external-assets/v4/icons/svg/ic_${iconName}_black_24px.svg`,
 			method: 'GET'
 		};
 		this.icon = {};
+		this.decoder = new StringDecoder('utf8');
 	}
-	
+
 	request() {
 		const req = https.request(this.options, (res) => {
 			// console.log('statusCode:', res.statusCode);
 	 		// console.log('headers:', res.headers);
 
 			res.on('data', (d) => {
-				this.parseXml(d);
+				const xmlString = this.decoder.write(d);
+				xml2js.parseString(xmlString, (err, result) => {
+					if(err) throw new Error(err);
+					this.icon = result;
+				});
 			});
 		});
 
@@ -27,12 +34,6 @@ class GetIcon {
 
 		req.end()
 	}
-
-	parseXml(d) {
-		console.log(typeof d);
-		this.icon = d;
-		process.stdout.write(d);
-	};
 }
 
 module.exports = GetIcon;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
+global.GetIcon = {};
 global.reLoadModule = () => {
 	delete require.cache[require.resolve('./get-icon.js')];
-	var GetIcon = require('./get-icon.js');
-	getIcon = new GetIcon();
+	GetIcon = require('./get-icon.js');
 }
+reLoadModule();
