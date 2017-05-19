a8ee2c4aebb56c1a89fa89b1e8e900573a2a82c9
Matt Harris <mattharris89@gmail.com>
2017-05-19 17:05:07 -0400

    Add icons file for testing actual adding, starting some work on prompting for CLI input and determining whether to replace an existing icon already in the file

--- src/get-icon.js
+++ src/get-icon.js
@@ -1,53 +1,98 @@
-const https = require('https');
-const xml2js = require('xml2js');
+const https = require("https");
+const readline = require("readline");
+const xml2js = require("xml2js");
+iconsJson = require("./Icons.json");
 
 class GetIcon {
-	constructor(iconName) {
-		this.options = {
-			hostname: 'storage.googleapis.com',
-			port: 443,
-			path: `/material-icons/external-assets/v4/icons/svg/ic_${iconName}_black_24px.svg`,
-			method: 'GET'
-		};
-		this.icon = {};
-	}
+  constructor(iconName) {
+    this.iconName = this.underScore(iconName);
+    this.options = {
+      hostname: "storage.googleapis.com",
+      port: 443,
+      path: `/material-icons/external-assets/v4/icons/svg/ic_${this.iconName}_black_24px.svg`,
+      method: "GET"
+    };
+    this.icon = {};
+    this.parsedPath = "";
+  }
 
-	request() {
-		const req = https.get(this.options, (res) => {
-			// console.log('statusCode:', res.statusCode);
-	 		// console.log('headers:', res.headers);
-			let xmlString = "";
-			res.setEncoding('utf8');
-			res.on('data', (d) => {
-				xmlString += d;
-			});
-			res.on('end', () => {
-				xml2js.parseString(xmlString, (err, result) => {
-					if(err) throw new Error(err);
-					this.icon = result;
-					const path = this.parsePath(result);
-					console.log(path);
-				});
-			});
-		});
+  underScore(name = "") {
+    if (!name) throw Error("Cant underscore delimit empty string...");
+    return name.replace(" ", "_");
+  }
 
-		req.on('error', (e) => {
-			console.error(e);
-		});
+  request() {
+    const req = https.get(this.options, res => {
+      // console.log('statusCode:', res.statusCode);
+      // console.log('headers:', res.headers);
+      let xmlString = "";
+      res.setEncoding("utf8");
+      res.on("data", d => {
+        xmlString += d;
+      });
+      res.on("end", () => {
+        xml2js.parseString(xmlString, (err, result) => {
+          if (err) throw new Error(err);
+          this.icon = result;
+          this.parsedPath = this.parsePath(result);
+          this.savePath(this.parsedPath);
+        });
+      });
+    });
 
-		req.end()
-	}
-	
-	parsePath(icon) {
-		const paths = icon.svg.path;
-		for(let i=0; i<paths.length; i++) {
-			let path = paths[i];
-			if(path.$.d != 'M0 0h24v24H0z' && !path.$.hasOwnProperty('fill')) {
-				return path.$.d; 
-			}
-		}
-	}
+    req.on("error", e => {
+      console.error(e);
+    });
 
+    req.end();
+  }
+
+  /**
+	 * Default to instance parsedPath
+	 */
+  savePath(path = this.parsedPath) {
+    if (!path) throw Error("No path to save passed and no path in memory.");
+    var upCased = this.iconName.toUpperCase();
+    if (iconsJson.hasOwnProperty(upCased)) {
+      this.prompt("This Icon is already stored, do you want to replace it? (y/n)");
+    }
+  }
+
+  prompt(text) {
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout
+    });
+
+    rl.question(text, answer => {
+      switch (answer) {
+        case "y":
+          console.log("sweet, lets do it.");
+          rl.close();
+          break;
+        case "n":
+          console.log("okay, cool. exiting.");
+          rl.close();
+          process.exit();
+          break;
+        default:
+          console.log('Sorry just type "y" or "n" lowercase, only input allowed for now.');
+          rl.close();
+          this.prompt(text); // re-prompt...
+          break;
+      }
+    });
+  }
+
+  parsePath(icon) {
+    const paths = icon.svg.path;
+    for (let i = 0; i < paths.length; i++) {
+      let path = paths[i];
+      if (path.$.d != "M0 0h24v24H0z" && !path.$.hasOwnProperty("fill")) {
+        return path.$.d;
+      }
+    }
+  }
 }
 
 module.exports = GetIcon;
